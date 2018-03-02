c545fcb5d65e5a21e29ae4c69c786bbc7c144856
Nam Pham <nam.phho@gmail.com>
2018-03-02 10:31:36 +0700

    [DEV] first commit

---
+++ app/src/main/java/com/example/exercise2/SettingBackgroundActivity.java
@@ -0,0 +1,13 @@
+package com.example.exercise2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingBackgroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_background);
+    }
+}
